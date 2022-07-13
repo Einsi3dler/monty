450062a097464b9be84469de217ecc14c15fe122
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2022-07-13 23:20:48 +0000

    fix

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "monty.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int main(int argc, char *argv)
+{
+	FILE *read_monty = fopen(argv[1], 'r');
+
+
+}
