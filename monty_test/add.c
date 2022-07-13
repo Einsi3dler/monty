377f2faa0ecfa765941b906329b01ededf2ed265
Rotimi Owolabi <iamrotimiowolabi@gmail.com>
2022-07-13 21:52:35 +0000

    test_case

--- monty_test/add.c
+++ monty_test/add.c
@@ -1,6 +1,6 @@
 #include "test.h"
 
-int add(int a, int b)
+void add(int a, int b)
 {
     int sum = a + b;
     printf("%d\n", sum);
--- monty_test/test.h
+++ monty_test/test.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-int add(int a, int b);
+void add(int a, int b);
 
 #endif
