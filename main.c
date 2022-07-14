61707216119f503bd59e1911a87455735c4112b0
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-14 00:19:11 +0000

    Main.c hotfix

--- main.c
+++ main.c
@@ -5,9 +5,33 @@
 #include <ctype.h>
 
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	FILE *read_monty = fopen(argv[1], 'r');
+	//Declearation of getline variables
+	char *buffer = NULL;
+	size_t buffsize = 0;
+	size_t characters;
 
+	//variable to store tokens
+	char *token = NULL;
+
+	FILE *read_monty; 
+	
+	read_monty = fopen(argv[1], "r");
+
+	while(getline(&buffer, &buffsize, read_monty)!= -1)
+	{
+		token = strtok(buffer, " /t/n");
+
+		while (token != NULL)
+		{
+			printf("%s\n",token);
+
+			token = strtok(NULL, " /t/n");
+		
+		}
+	}
+
+	return 0;
 
 }
