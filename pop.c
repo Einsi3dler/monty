3203e4dcc66df32cc2555d22dab525250c328cb6
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-07-16 19:29:42 +0000

    fix: stack Task Updated

--- pop.c
+++ pop.c
@@ -8,17 +8,17 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *holder;
+	stack_t *holder = *stack;
 
-	holder = *stack;
-	if (!holder)
+	if (!(*stack))
 	{
 		printf("L<%d>: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else
+
+	else if (holder)
 	{
+		*stack = holder->next;
 		free(holder);
 	}
-	*stack = (*stack)->next;
 }
